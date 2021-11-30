da4eca9b65d1aabbee43fe6e9ac7a379055df9df
INSORTIAL <renwell2099@gmail.com>
2021-11-29 21:27:06 -0800

    Created read line function

--- main.c
+++ main.c
@@ -1,4 +1,64 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#define LSH_RL_BUFSIZE 1024
+char *lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if(!buffer) 
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) 
+    {
+        c = getchar();
+
+        if(c == EOF || c == '\n') 
+        {
+            buffer[position] = '\0';
+            return buffer;
+        } else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if(position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if(!buffer)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do 
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
 
 int main(int argc, char **argv)
 {
